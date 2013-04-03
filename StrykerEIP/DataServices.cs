ca817699a8f58f16e4777e6ab9d457ad1b208173
Chuck Blaine <eiviery@gmail.com>
2013-04-03 00:16:53 -0400

    merged changes from master

--- StrykerEIP/DataServices.cs
+++ StrykerEIP/DataServices.cs
@@ -29,6 +29,19 @@ namespace StrykerEIP
                 using(SqlDataAdapter da = new SqlDataAdapter(cmd))
                 {
                     DataSet dataSetBusinessProcess = new DataSet();
+                    da.TableMappings.Add("KPI1", "RawData1");
+                    da.TableMappings.Add("KPI2", "RawData2");
+                    da.TableMappings.Add("KPI3", "RawData3");
+                    da.TableMappings.Add("KPI4", "RawData4");
+
+                    da.TableMappings.Add("KPI5", "KPIResult1");
+                    da.TableMappings.Add("KPI6", "KPIResult2");
+                    da.TableMappings.Add("KPI7", "KPIResult3");
+                    da.TableMappings.Add("KPI8", "KPIResult4");
+
+                    da.TableMappings.Add("KPI9", "States");
+                    da.TableMappings.Add("KPI10", "Decisions");
+
                     da.Fill(dataSetBusinessProcess, "KPI");
 
                     return dataSetBusinessProcess;
